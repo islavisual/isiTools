079beccd8b936d96f0dbd30e9a7670e8bd493876
Isla Visual <islavisual@gmail.com>
2019-04-04 14:51:33 +0200

    Update isiTools.js
    Fixed Striptags

--- isiTools.js
+++ isiTools.js
@@ -2704,7 +2704,7 @@ function isiToolsCallback(json){
 
 			allowed = (((allowed || '') + '').toLowerCase().match(/<[a-z][a-z0-9]*>/g) || []).join('');
 			var tags = /<\/?([a-z][a-z0-9]*)\b[^>]*>/gi, commentsAndPhpTags = /<!--[\s\S]*?-->|<\?(?:php)?[\s\S]*?\?>/gi;
-			return input.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
+			return inp.replace(commentsAndPhpTags, '').replace(tags, function ($0, $1) {
 				return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : '';
 			});
 		}
